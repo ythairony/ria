d720c4179ef229be9e5618ce1bcc408c8efe15be
Yuri Thairony <yurithairony@hotmail.com>
2023-09-24 09:42:38 -0300

    script pro seminário

---
+++ seminario typescript/classes.ts
@@ -0,0 +1,69 @@
+class Carro {
+    // Atributos
+    _marca: string;
+    _modelo: string;
+    _cor: string;
+    _anoDeFabricacao: number;
+    
+    //Construtor
+    constructor(marca:string, modelo:string, cor:string, anoDeFabricacao:number) {
+        this._marca = marca;
+        this._modelo = modelo;
+        this._cor = cor;
+        this._anoDeFabricacao = anoDeFabricacao;
+    }
+
+    // Acessadores
+    get marca() {
+        return this._marca;
+    }
+
+    get modelo() {
+        return this._modelo;
+    }
+
+    get cor() {
+        return this._cor;
+    }
+
+    get anoDeFabricacao() {
+        return this._anoDeFabricacao;
+    }
+
+    set marca(marca) {
+        this._marca = marca;
+    }
+
+    set modelo(modelo) {
+        this._modelo = modelo;
+    }
+
+    set cor(cor) {
+        this._cor = cor;
+    }
+
+    set anoDeFabricacao(anoDeFabricacao) {
+        if (anoDeFabricacao > 1885) {
+            this._anoDeFabricacao = anoDeFabricacao;
+        } else { 
+            throw new Error('Não existe carros criados antes de 1885');
+        }
+    }
+
+    //Métodos
+    acelerar(velocidade:number):string {
+        return `${this.veiculo()} está acelerando a ${velocidade}km/h`
+    }
+
+    frear():string {
+        return `${this.veiculo} freiou`
+    }
+
+    virar(direcao: 'direita' | 'esquerda'):string {
+        return `${this.veiculo} está virando à ${direcao}`
+    }
+
+    veiculo():string {
+        return this._modelo;
+    }
+}
